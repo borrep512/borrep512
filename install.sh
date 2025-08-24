cac5029b306a74cbf9fb3b045b43aae13188a0fc
borrep512 <borrep512@proton.me>
2025-08-24 17:55:37 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,11 @@
-loadkeys es   # Cambiar teclado a español
-ping archlinux.org -c 5  # Probar conexión a internet
-echo "[+] Hay conexion, se procede a instalar."
+#!/bin/bash
+
+loadkeys es
+ping archlinux.org -c 5
+echo "[+] Hay conexión, se procede a instalar."
 sleep 5
 
-#Particiones
+# --- Particionado ---
 parted /dev/sda --script mklabel gpt
 parted /dev/sda --script mkpart ESP fat32 1MiB 301MiB
 parted /dev/sda --script set 1 esp on
@@ -12,7 +14,7 @@ parted /dev/sda --script mkpart primary ext4 2301MiB 100%
 echo "[+] Particiones 1 completado."
 sleep 5
 
-#Particiones 2
+# --- Formateo ---
 mkfs.fat -F32 /dev/sda1
 mkswap /dev/sda2
 swapon /dev/sda2
@@ -20,75 +22,74 @@ mkfs.ext4 /dev/sda3
 echo "[+] Particiones 2 completado."
 sleep 5
 
-#Particiones 3
+# --- Montaje ---
 mount /dev/sda3 /mnt
-mkdir /mnt/boot
+mkdir -p /mnt/boot
 mount /dev/sda1 /mnt/boot
 echo "[+] Particiones 3 completado."
 sleep 5
 
-#Pseudsistemas
-mkdir -p /mnt/proc
-mkdir -p /mnt/sys
-mkdir -p /mnt/dev
-mkdir -p /mnt/run
-mkdir -p /mnt/tmp
+# --- Preparar pseudo-sistemas ---
+mkdir -p /mnt/{proc,sys,dev,run,tmp}
 mount --types proc /proc /mnt/proc
 mount --rbind /sys /mnt/sys
 mount --make-rslave /mnt/sys
 mount --rbind /dev /mnt/dev
 mount --make-rslave /mnt/dev
-mount --bind /run /mnt/run  # opcional pero recomendable
+mount --bind /run /mnt/run
 echo "[+] Pseudosistemas montados."
-sleep 10
-
-
-#Instalaciones
-arch-chroot /mnt pacman -S --noconfirm base
-echo "[+] Base instalado."
-sleep 15
-arch-chroot /mnt pacman -S --noconfirm linux
-echo "[+] Linux instalado."
-sleep 15
-arch-chroot /mnt pacman -S --noconfirm linux-firmware
-echo "[+] Linux firmware instalado."
-sleep 15
-arch-chroot /mnt pacman -S --noconfirm vim
-echo "[+] Vim instalado."
-sleep 15
-arch-chroot /mnt pacman -S --noconfirm networkmanager
-echo "[+] NetworkManager instalado."
-sleep 15
-
-#Hora
-ln -sf /usr/share/zoneinfo/Europe/Madrid /etc/localtime
-hwclock --systohc
-echo "[+] Hora establecida."
 sleep 5
 
-#Host y usuarios
+# --- Copiar pacman temporal para instalar base por partes ---
+mkdir -p /mnt/usr/bin /mnt/etc
+cp -a /usr/bin/pacman /mnt/usr/bin/
+cp -a /etc/pacman.conf /mnt/etc/
+
+# --- Instalar base por partes ---
+arch-chroot /mnt pacman -S --noconfirm bash coreutils filesystem
+echo "[+] Base mínimo instalado."
+sleep 5
+arch-chroot /mnt pacman -S --noconfirm linux
+echo "[+] Linux instalado."
+sleep 5
+arch-chroot /mnt pacman -S --noconfirm linux-firmware
+echo "[+] Linux firmware instalado."
+sleep 5
+arch-chroot /mnt pacman -S --noconfirm vim
+echo "[+] Vim instalado."
+sleep 5
+arch-chroot /mnt pacman -S --noconfirm networkmanager
+echo "[+] NetworkManager instalado."
+sleep 5
+
+# --- Configuración dentro del chroot ---
+arch-chroot /mnt /bin/bash <<EOF
+
+# Hora
+ln -sf /usr/share/zoneinfo/Europe/Madrid /etc/localtime
+hwclock --systohc
+
+# Locales
+sed -i 's/#es_ES.UTF-8 UTF-8/es_ES.UTF-8 UTF-8/' /etc/locale.gen
+locale-gen
+echo "LANG=es_ES.UTF-8" > /etc/locale.conf
+
+# Host y usuarios
 echo "herculerch" > /etc/hostname
 useradd -m -G wheel -s /bin/bash herculex
 echo "root:123456" | chpasswd
 echo "herculex:123456" | chpasswd
-echo "[+] Ajustes de host y de usuarios completados."
-sleep 5
+sed -i 's/# %wheel ALL=(ALL:ALL) ALL/%wheel ALL=(ALL:ALL) ALL/' /etc/sudoers
 
-#Grub
-pacman -S grub efibootmgr --noconfirm
-sleep 15
+# Instalar GRUB y habilitar NetworkManager
+pacman -S --noconfirm grub efibootmgr
 grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=Arch
-sleep 15
 grub-mkconfig -o /boot/grub/grub.cfg
-sleep 15
-echo "[+] Grub instalado."
 
-#Network
-pacman -S --noconfirm networkmanager
 systemctl enable NetworkManager
-echo "[+] Network manager instalado y operativo."
-sleep 15
 
-exit
+EOF
+
+# --- Desmontar todo ---
 umount -R /mnt
-echo "[+] Todo listo, ya puedes quitar salir y quitar la ISO."
+echo "[+] Todo listo, ya puedes reiniciar y quitar la ISO."
