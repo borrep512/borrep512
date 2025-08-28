cc829481c97a9ca5134fb02f5f5a01601eb9fc81
borrep512 <borrep512@proton.me>
2025-08-28 09:01:57 +0200

    Update postinstall.sh

--- postinstall.sh
+++ postinstall.sh
@@ -1,36 +1,36 @@
 #--------------------
 #1. Xorg al mínimo
 #--------------------
-sudo pacman -Syu
-sudo pacman -S xorg-server 
-sudo pacman -S xorg-xinit
-sudo pacman -S xorg-xrandr
-sudo pacman -S xorg-xsetroot
-sudo pacman -S xorg-xprop
-sudo pacman -S xorg-xev
+sudo pacman -Sy
+sudo pacman -S xorg-server --noconfirm
+sudo pacman -S xorg-xinit --noconfirm
+sudo pacman -S xorg-xrandr --noconfirm
+sudo pacman -S xorg-xsetroot --noconfirm
+sudo pacman -S xorg-xprop --noconfirm
+sudo pacman -S xorg-xev --noconfirm
 
 #-----------
 #2. Instalación de entorno (bspwn, sxhkd, polybar, picom, dunst, feh, thunar, pavucontrol, brightnessctl, kitty, zsh, xclip, xsel, virtualbox-services, network fuentes)
 #-----------
-sudo pacman -S bspwm sxhkd
-sudo pacman -S polybar
-sudo pacman -S picom
-sudo pacman -S dunst
-sudo pacman -S feh
-sudo pacman -S thunar thunar-volman gvfs gvfs-smb 
-sudo pacman -S pavucontrol 
-sudo pacman -S brightnessctl
-sudo pacman -S kitty
-sudo pacman -S zsh
-sudo pacman -S xclip xsel
-sudo pacman -S virtualbox-guest-utils linux-headers
-sudo pacman -S networkmanager network-manager-applet nm-connection-editor
-sudo pacman -S ttf-dejavu ttf-liberation noto-fonts noto-fonts-emoji
+sudo pacman -S bspwm sxhkd --noconfirm
+sudo pacman -S polybar --noconfirm
+sudo pacman -S picom --noconfirm
+sudo pacman -S dunst --noconfirm
+sudo pacman -S feh --noconfirm
+sudo pacman -S thunar thunar-volman gvfs gvfs-smb --noconfirm
+sudo pacman -S pavucontrol --noconfirm
+sudo pacman -S brightnessctl --noconfirm
+sudo pacman -S kitty --noconfirm
+sudo pacman -S zsh --noconfirm
+sudo pacman -S xclip xsel --noconfirm
+sudo pacman -S virtualbox-guest-utils linux-headers --noconfirm
+sudo pacman -S networkmanager network-manager-applet nm-connection-editor --noconfirm
+sudo pacman -S ttf-dejavu ttf-liberation noto-fonts noto-fonts-emoji --noconfirm
 #----------
 #3. Instalación de herramientas ()
 #-------------
-sudo pacman -S git 
-sudo pacman -S wget
-sudo pacman -S curl 
-sudo pacman -S neofetch
-sudo pacman -S htop
+sudo pacman -S git --noconfirm
+sudo pacman -S wget --noconfirm
+sudo pacman -S curl --noconfirm
+sudo pacman -S neofetch --noconfirm
+sudo pacman -S htop --noconfirm
