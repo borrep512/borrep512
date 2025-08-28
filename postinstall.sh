b4bba0a3eff607b4ca827f6d752244cb307f38d5
borrep512 <borrep512@proton.me>
2025-08-28 09:04:48 +0200

    Update postinstall.sh

--- postinstall.sh
+++ postinstall.sh
@@ -8,10 +8,11 @@ sudo pacman -S xorg-xrandr --noconfirm
 sudo pacman -S xorg-xsetroot --noconfirm
 sudo pacman -S xorg-xprop --noconfirm
 sudo pacman -S xorg-xev --noconfirm
+sleep 10
 
-#-----------
-#2. Instalación de entorno (bspwn, sxhkd, polybar, picom, dunst, feh, thunar, pavucontrol, brightnessctl, kitty, zsh, xclip, xsel, virtualbox-services, network fuentes)
-#-----------
+#--------------------------------------------------------------------------------
+#2. Instalación de entorno (bspwn, sxhkd, polybar, picom, dunst, feh, thunar, pavucontrol, brightnessctl, kitty, zsh, lightdm xclip, xsel, virtualbox-services, network fuentes)
+#--------------------------------------------------------------------------------
 sudo pacman -S bspwm sxhkd --noconfirm
 sudo pacman -S polybar --noconfirm
 sudo pacman -S picom --noconfirm
@@ -22,15 +23,27 @@ sudo pacman -S pavucontrol --noconfirm
 sudo pacman -S brightnessctl --noconfirm
 sudo pacman -S kitty --noconfirm
 sudo pacman -S zsh --noconfirm
+sudo pacman -S lightdm --noconfirm
 sudo pacman -S xclip xsel --noconfirm
 sudo pacman -S virtualbox-guest-utils linux-headers --noconfirm
 sudo pacman -S networkmanager network-manager-applet nm-connection-editor --noconfirm
 sudo pacman -S ttf-dejavu ttf-liberation noto-fonts noto-fonts-emoji --noconfirm
-#----------
-#3. Instalación de herramientas ()
-#-------------
+sleep 10
+
+#----------------------------------------------------------------
+#3. Instalación de herramientas (git, wget, curl, neofetch, htop)
+#----------------------------------------------------------------
 sudo pacman -S git --noconfirm
 sudo pacman -S wget --noconfirm
 sudo pacman -S curl --noconfirm
 sudo pacman -S neofetch --noconfirm
 sudo pacman -S htop --noconfirm
+
+
+#------------------------------------------------------------
+#4. Habilitar servicios (NetworkManager, lightdm, sshd, cups)
+#------------------------------------------------------------
+sudo systemctl enable NetworkManager
+sudo systemctl enable lightdm
+sudo systemctl enable sshd
+sudo systemctl enable org.cups.cupsd
