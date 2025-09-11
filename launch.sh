b882264a154bf16e134e34913402a99b89245bc8
borrep512 <borrep512@proton.me>
2025-09-11 20:29:19 +0200

    Delete launch.sh

--- launch.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-# Terminar instancias previas
-killall -q polybar
-
-# Esperar que terminen
-while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
-
-# Lanzar barra principal
-polybar main &
